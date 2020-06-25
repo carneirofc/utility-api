847382d24d85b5b5957a65a52a3ee5242bb14647
carneirofc <claudiofcarneiro@hotmail.com>
2020-06-24 21:33:53 -0300

    Include LDAP_BINDPASS and SECRET_KEY to config.py

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
 """Flask config class."""
 import os
+import secrets
 
 
 class Config:
@@ -9,8 +10,10 @@ class Config:
     TESTING = False
     USE_RELOADER = False
 
-    SECRET_KEY = os.environ.get("SECRET_KEY", "")
+    SECRET_KEY = os.environ.get("SECRET_KEY", secrets.token_urlsafe(16))
     REDIS_URL = os.environ.get("REDIS_URL", "redis://redis:6379/0")
+
+    LDAP_BINDPASS = os.environ.get("LDAP_BINDPASS", "")
 
     """ This can be an url or a filesystem path.
         If it's a filesystem path, it will automatically be updated when the file changes. """
