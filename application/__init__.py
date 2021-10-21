3f309a48e17f703a6b5726fb3cc9e799c2446e68
carneirofc <claudiofcarneiro@hotmail.com>
2021-10-21 20:17:09 -0300

    Application config cleanup

--- application/__init__.py
+++ application/__init__.py
@@ -7,27 +7,44 @@ redis_client = FlaskRedis()
 def create_app():
 
     app = Flask(__name__, instance_relative_config=False)
-    app.config.from_object("config.Config")
+    app.config.from_object("config.ProductionConfig")
 
     with app.app_context():
         redis_client.init_app(app)
 
-        from application.spreadsheet.backend import BackendServer
+        if app.config.get("ENABLE_SPREADSHEET", False):
+            _app_register_spreadsheet(app)
 
-        backendServer = BackendServer()
-        backendServer.start()
+        if app.config.get("ENABLE_LDAP", False):
+            _app_register_ldap(app)
 
-        from application.spreadsheet.client import SyncService
-
-        syncService = SyncService()
-        syncService.start()
-
-        import application.spreadsheet.routes as spreadsheet_routes
-
-        app.register_blueprint(spreadsheet_routes.spreadsheet_bp)
-
-        import application.ldap_bp.routes as ldap_routes
-
-        app.register_blueprint(ldap_routes.ldap_bp)
+        if app.config.get("ENABLE_JWT_TOKEN", False):
+            _app_register_jwt_token(app)
 
         return app
+
+
+def _app_register_jwt_token(app):
+    pass
+
+
+def _app_register_spreadsheet(app):
+    from application.spreadsheet.backend import BackendServer
+
+    backendServer = BackendServer()
+    backendServer.start()
+
+    from application.spreadsheet.client import SyncService
+
+    syncService = SyncService()
+    syncService.start()
+
+    import application.spreadsheet.routes as spreadsheet_routes
+
+    app.register_blueprint(spreadsheet_routes.spreadsheet_bp)
+
+
+def _app_register_ldap(app):
+    import application.ldap_bp.routes as ldap_routes
+
+    app.register_blueprint(ldap_routes.ldap_bp)
--- config.py
+++ config.py
@@ -3,15 +3,42 @@ import os
 import secrets
 
 
-class Config:
-    """Base config vars."""
+class BaseConfig:
+    ENABLE_SPREADSHEET = False
+    ENABLE_LDAP = False
+    ENABLE_JWT_TOKEN = False
 
     DEBUG = False
     TESTING = False
     USE_RELOADER = False
 
-    # SECRET_KEY = os.environ.get("SECRET_KEY", secrets.token_urlsafe(16))
-    SECRET_KEY = secrets.token_urlsafe(16)
+    SECRET_KEY = os.environ.get("SECRET_KEY", secrets.token_urlsafe(16))
+
+
+class LDAPDevelopmentConfig:
+    ENABLE_LDAP = True
+
+    DEBUG = True
+    TESTING = True
+    USE_RELOADER = True
+
+    LDAP_BINDPASS = os.environ.get("LDAP_BINDPASS", "")
+
+
+class JWTDevelopmentConfig:
+    ENABLE_JWT_TOKEN = True
+
+    DEBUG = True
+    TESTING = True
+    USE_RELOADER = True
+
+
+class ProductionConfig(BaseConfig):
+    """Base config vars."""
+
+    ENABLE_SPREADSHEET = True
+    ENABLE_LDAP = True
+
     REDIS_URL = os.environ.get("REDIS_URL", "redis://redis:6379/0")
 
     LDAP_BINDPASS = os.environ.get("LDAP_BINDPASS", "")
